84acc5cc912bfaf78115807fe2d847be2e906f5f
leodelbrio <leonardodelbicho@gmail.com>
2024-11-18 18:32:29 -0300

    comienzo matris

--- appTemp.php
+++ appTemp.php
@@ -1,5 +1,12 @@
 <?php
+/* Un organismo provincial desea contar con la información sobre las temperaturas máximas promedio mensuales de la ciudad de Neuquén considerando los últimos 10 años, para poder obtener estadísticas y responder a consultas varias.
+Para ello se almacenan los valores de temperaturas en un arreglo bidimensional (matriz), tomando una fila por año, desde 2014 a 2023, y una columna por mes.
+*/
 
 
+//Matris de las temperaturas 
+function matrisFechas (){
+
+};
 
 ?>
