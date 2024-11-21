49d9a8bf806d02855992bb474b468c4590820948
Naxo <joaquin.castillo@est.fi.uncoma.edu.ar>
2024-11-20 22:03:10 -0300

    Solucion de bugs. Apariencia del modulo C y discriminacion del modulo B

--- appTemp.php
+++ appTemp.php
@@ -28,26 +28,80 @@ function matrizFechas (){
 
 function matrizManual ($arraysTemps){
     /**PUNTO B */
+    $temp = 0;
     for ($i = 0; $i < 10; $i++) {
         for ($j = 0; $j < 12; $j++){
             echo ("Ingrese la temperatura para el año: ". $i + 2014 . " y el mes: ". $j + 1 . ". \n");
             $temp = trim(fgets(STDIN));
+            if ($temp < -50 || $temp > 70){
+                while ($temp < -50 || $temp > 70){
+                echo("Ingrese una temperatura valida (Mayor a -50 y menor a 70): ");
+                $temp = trim(fgets(STDIN));
+            }
+            }
             $arraysTemps[$i][$j] = $temp; 
         }
+        
+        }
+        return $arraysTemps;
     };
-    return $arraysTemps;
-}
+
 function mostrarMatrizCompleta ($arraysTemps){
     /**PUNTO C */
-
+    $anioColum = 2013;
+    for($v = 0; $v < 12; $v++){
+        switch ($v){
+            case 0:
+                echo ("        ene ");
+                break;
+            case 1:
+                echo ("feb ");
+                break;
+            case 2:
+                echo ("mar ");
+                break;
+            case 3:
+                echo ("abr ");
+                break;
+            case 4:
+                echo ("may ");
+                break;
+            case 5:
+                echo ("jun ");
+                break;
+            case 6:
+                echo ("jul ");
+                break;
+            case 7:
+                echo ("ago ");
+                break;
+            case 8:
+                echo ("sep ");
+                break;
+            case 9:
+                echo ("oct ");
+                break;
+            case 10:
+                echo ("nov ");
+                break;
+            case 11:
+                echo ("dic \n");
+                break;
+                    }
+    }
+    
+    echo("      --------------------------------------------------\n");
     for ($i = 0; $i < 10; $i++) {
+        $anioColum ++;
+        echo ($anioColum. " |");
         for ($j = 0; $j < 12; $j++){
-            echo " " . $arraysTemps[$i][$j];
+            echo "  " . str_pad($arraysTemps[$i][$j], 2, "0", STR_PAD_LEFT);
         };
         echo "\n";
+        
     };
-};
 
+}
 function temperaturaPedida ($arraysTemps, $anio, $mes){
     /**PUNTO D */
     $colum = $anio - 2014;
@@ -69,7 +123,7 @@ function mostrarMatrizMes ($arraysTemps, $mes){
     /**PUNTO F */
     $suma = 0;
     for ($j = 0; $j < 10; $j++){
-        echo " " . $arraysTemps[$j][$mes - 1];
+        echo " " . $arraysTemps[$j][$mes];
         $suma = $suma + $arraysTemps[$j][$mes];
     };
     $suma = $suma / 10;
@@ -103,41 +157,40 @@ function tempMaxYMin ($arraysTemps){
     echo("\nEl maximo es: ". $max. " grados del año ". $anioMax. " del mes ". $mesMax + 1 . "\n");
 }; 
 
-function primavera($arraysTemps){
+function primavera(){
     /**PUNTO H */
-
-    for ($i = 0; $i < 10; $i++) {
-        for ($j = 9; $j < 12; $j++){
-            echo " " . $arraysTemps[$i][$j];
-        };
-        echo "\n";
-    };    
-};
-
-function invierno($arraysTemps){
-    /**PUNTO I*/
-    for ($i = 5; $i < 10; $i++) {
-        for ($j = 6; $j < 9; $j++){
-            echo " " . $arraysTemps[$i][$j];
-        };
-        echo "\n";
-    }; 
-};
-
-function mostrarTodo ($arraysTemps){
-    $matrizTemperaturas = array (
-        array ("Completo" . "\n" =>  mostrarMatrizCompleta ($arraysTemps)),
-        array ("Primavera" . "\n" => primavera($arraysTemps)),
-        array ("Invierno" . "\n" => invierno($arraysTemps)),
+    $matrizPrimavera = array(
+        array(20, 25, 29),
+        array(21, 26, 31),
+        array(21, 27, 32),
+        array(22, 26, 31),
+        array(20, 24, 30),
+        array(23, 25, 29),
+        array(22, 27, 29),
+        array(21, 28, 30),
+        array(22, 26, 30),
+        array(23, 28, 31)
     );
+    echo mostrarMatrizCompleta($matrizPrimavera) . "\n";
+    
+};
 
-    return $matrizTemperaturas;
+function invierno(){
+    /**PUNTO I*/
+    $matrizInvernal = array(
+        array(12, 11, 17),
+        array(10, 12, 16),
+        array(11, 13, 17),
+        array(11, 15, 18),
+        array(13, 15, 19)
+    );
+    echo mostrarMatrizCompleta($matrizInvernal) . "\n";
 };
 
 $s = 0;
 
 while ($s != 11) {
-echo "\n" . "1 - Realizar una carga automática de la matriz de temperaturas." . "\n";
+echo "1 - Realizar una carga automática de la matriz de temperaturas." . "\n";
 echo "2 - Realizar una carga manual de la matriz de temperaturas." . "\n";
 echo "3 - Mostrar el contenido de la matriz por filas y columnas." . "\n";
 echo "4 - Mostrar, dado un año y un mes, el valor de temperatura correspondiente." . "\n";
@@ -193,14 +246,13 @@ $s = trim(fgets(STDIN));
             echo tempMaxYMin($arraysTemps);
             break;
         case 8: 
-            echo primavera($arraysTemps);
+            echo primavera();
             break;
         case 9:
-            echo invierno($arraysTemps);    
+            echo invierno();    
             break;
         case 10:
-            $mostrarTodoTemp = mostrarTodo ($arraysTemps);
-            print_r($mostrarTodoTemp);
+            echo matrizCompleta ();
             break;
         case 11:
             $s = 11;
